1d1608e84220c53eb6e2d19aedd877eb099f5c2d
kevinlustig <kevin.lustig@gmail.com>
2022-11-01 12:56:39 +0000

    Turning on debugging

--- wp-config.php
+++ wp-config.php
@@ -70,7 +70,10 @@ $table_prefix = 'wp_';
  * "true" in dev, but false in test and live.
  */
 if ( ! defined( 'WP_DEBUG' ) ) {
-	define('WP_DEBUG', false);
+	define( 'WP_DEBUG', true );
+  define( 'WP_DEBUG_LOG', true );
+  define( 'WP_DEBUG_DISPLAY', true );
+  define( 'WP_CACHE', false );
 }
 
 /* That's all, stop editing! Happy Pressing. */

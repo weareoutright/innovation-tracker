22b733e0708ba8adc00867a7c5ff0392f396963e
kevinlustig <kevin.lustig@gmail.com>
2022-11-16 13:29:58 +0000

    Revert "Setting up new project"
    This reverts commit 950834e0139dc2f33ccf76670a1839d6907fe035.

---
+++ wp-content/themes/innovation-tracker-2022-child/build/js/nav-jump.js
@@ -0,0 +1,50 @@
+(($) => {
+  const target = $('.nav-jump');
+  const initialized = target.hasClass("initialized");
+  if (!initialized) {
+    target.addClass("initialized");
+
+    let topTimeout;
+    const $body = $('body');
+    const $window = $(window);
+    const $buttons = target.find(".wp-block-group__inner-container");
+    const $clone = $buttons.clone();
+    const $stickyWrapper = $("<div class='nav-jump sticky'>");
+    const $container = $("<div class='container'></div>");
+    $stickyWrapper.append($container);
+    $container.append($clone);
+
+    let sticky = false;
+
+    const setSticky = (on) => {
+      if (sticky !== on) {
+        sticky = on;
+        if (on) {
+          $body.append($stickyWrapper);
+        } else {
+          $stickyWrapper.remove();
+        }
+      }
+    }
+
+    const setTop = () => {
+      if (topTimeout) clearTimeout(topTimeout);
+      topTimeout = setTimeout(() => {
+        setSticky($buttons.offset().top - $window.scrollTop() <= 0);
+      },1);
+    }
+
+    setTop();
+
+    window.addEventListener("scroll",setTop);
+    window.addEventListener("resize",setTop);
+
+    const resizeObserver = new ResizeObserver(entries => {
+      setTop();
+    });
+
+    // start observing a DOM node
+    resizeObserver.observe(document.body);
+  }
+})(jQuery);
+//# sourceMappingURL=nav-jump.js.map

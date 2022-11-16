950834e0139dc2f33ccf76670a1839d6907fe035
kevinlustig <kevin.lustig@gmail.com>
2022-11-16 13:19:48 +0000

    Setting up new project

---
+++ wp-content/themes/impactreport-2022-child/build/inc/post-types.php
@@ -0,0 +1,69 @@
+<?php
+
+trait ThemePostTypes {
+
+  function register_post_types() {
+
+    $DEFAULT_SUPPORTS = [ 'title', 'editor', 'custom-fields', 'excerpt', 'thumbnail'];
+
+    $post_types = [
+      // (object) [
+      //   'single' => 'Impact',
+      //   'plural' => 'Impacts',
+      //   'slug' => 'our-impact',
+      //   'id' => 'impact',
+      //   'icon' => 'superhero-alt',
+      //   'supports' => $DEFAULT_SUPPORTS,
+      //   'taxonomies' => ['strategy']
+      // ], 
+    ];
+
+    usort($post_types,function($a,$b) {
+      if ($a->single < $b->single) return -1;
+      if ($a->single > $b->single) return 1;
+      return 0;
+    });
+
+    foreach($post_types as $t=>$type) {
+      $id = property_exists($type,'id') ? $type->id : $type->slug;
+      $opts = [
+        'labels' => [
+          'name' => __( $type->plural, 'theme' ), /* This is the Title of the Group */
+          'singular_name' => __( $type->single, 'theme' ), /* This is the individual type */
+          'all_items' => __( 'All '.$type->plural, 'theme' ), /* the all items menu item */
+          'add_new' => __( 'Add New', 'theme' ), /* The add new menu item */
+          'add_new_item' => __( 'Add New '.$type->single, 'theme' ), /* Add New Display Title */
+          'edit' => __( 'Edit', 'theme' ), /* Edit Dialog */
+          'edit_item' => __( 'Edit '.$type->single, 'theme' ), /* Edit Display Title */
+          'new_item' => __( 'New '.$type->single, 'theme' ), /* New Display Title */
+          'view_item' => __( 'View '.$type->single, 'theme' ), /* View Display Title */
+          'search_items' => __( 'Search '.$type->plural, 'theme' ), /* Search Custom Type Title */
+          'not_found' =>  __( 'Nothing found in the Database.', 'theme' ), /* This displays if there are no entries yet */
+          'not_found_in_trash' => __( 'Nothing found in Trash', 'theme' ), /* This displays if there is nothing in the trash */
+          'parent_item_colon' => ''
+        ], /* end of arrays */
+        'description' => __( 'Theme '.$type->plural, 'theme' ), /* Custom Type Description */
+        'public' => true,
+        'publicly_queryable' => true,
+        'exclude_from_search' => false,
+        'show_ui' => true,
+        'query_var' => true,
+        'menu_icon' => 'dashicons-'.$type->icon, /* the icon for the custom post type menu */
+        'rewrite' => [ 'slug' => $type->slug ], /* you can specify its url slug */
+        'has_archive' => property_exists($type,'has_archive') ? $type->has_archive : $type->slug , /* you can rename the slug here */
+        'capability_type' => 'post',
+        'hierarchical' => false,
+        'show_in_rest' => true,
+        'supports' => $type->supports,
+        'taxonomies' => property_exists($type,'taxonomies') ? $type->taxonomies : array('post_tag','category'),
+        //'supports' => array( 'title', 'editor', 'author', 'thumbnail', 'excerpt', 'trackbacks', 'custom-fields', 'comments', 'revisions', 'sticky')
+      ]; /* end of options */
+      if (isset($type->opts)) $opts = array_merge($opts,$type->opts);
+      register_post_type($id,$opts);
+      //For some reason, thumbnail support isn't working for CPTs - add manually for each
+      if (in_array('thumbnail',$type->supports)) add_theme_support( 'post-thumbnails', [$id] ); 
+    }
+  }
+}
+
+?>
---
+++ wp-content/themes/impactreport-2022-child/src/single.php
@@ -0,0 +1,21 @@
+<?php 
+  global $ThemeSite;
+  $context = Timber::context();
+  $context['post'] = Timber::get_post();
+
+  $type = $context['post']->post_type;
+
+  $post_file = dirname(__DIR__).'/build/inc/posts/'.$type.'.php';
+  if(file_exists($post_file)) include $post_file;
+
+  $context['latest_posts'] = Timber::get_posts(array_merge(DEFAULT_POST_QUERY_ARGS, [
+    'posts_per_page' => 3
+  ]));
+
+  $context['block_back_to_top'] = $ThemeSite->get_global_block('Return to Top');
+  
+  Timber::render([
+    'single-'.$context['post']->post_type.'.twig',
+    'single.twig'
+  ], $context);
+?>
